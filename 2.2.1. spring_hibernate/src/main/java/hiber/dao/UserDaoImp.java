5cb4566139baf5b3ca308c10b0e70cb7963dc52e
Ilya Ershov <yellowspi@gmail.com>
2023-08-27 20:25:24 +0300

    delet oml

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
